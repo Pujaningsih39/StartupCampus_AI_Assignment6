f0f334057813754243d2f4f03862bac3e76f5e40
unknown <pujaningsih39@gmail.com>
2023-05-15 16:28:40 +0700

    Added multiplication and division functions

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from math_function import add
+from math_function import add, multiply, divide
 
 def main():
 
-    data_1 = int(input(masukkan input 1 :))
-    data_2 = int(input(masukkan input 2 :))
-    operator = input(masukkan operator :)
+    data_1 = int(input(Enter input 1: ))
+    data_2 = int(input(Enter input 2: ))
+    operator = input(Enter operator: )
 
     if operator == +:
         result = add(data_1, data_2)
+    elif operator == *:
+        result = multiply(data_1, data_2)
+    elif operator == /:
+        result = divide(data_1, data_2)
+    else:
+        print(Invalid operator)
 
     print({} {} {} = {} .format(data_1, operator, data_2, result))
 
 
 if __name__ == __main__:
-    print(Hello Main !)
     main()
+
--- math_function.py
+++ math_function.py
@@ -1,2 +1,9 @@
 def add(a, b):
     return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
